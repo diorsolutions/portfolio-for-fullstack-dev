662884d6bf9c18ba7c57e8d277160f9bb67381ee
diorsolutions <diorliders@gmail.com>
2026-03-29 01:53:03 +0500

    spalsh added

--- app/layout.tsx
+++ app/layout.tsx
@@ -224,7 +224,7 @@ export default function RootLayout({
         className={` select-none ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div className="hidden lg:block">
-          {/* <SplashCursor /> */}
+          <SplashCursor />
         </div>
 
         {children}
